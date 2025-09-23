7fc4ab7426c424f7f1893f667a8f813dcc8b250e
Kristoffer <kristoffer.ohlund@icloud.com>
2025-09-23 13:44:39 +0200

    Update CreateItem component

--- src/components/CreateItem.vue
+++ src/components/CreateItem.vue
@@ -1,52 +1,44 @@
 <script setup lang="ts">
-  import { ref } from 'vue';
-  import { API } from '../config/config.js';
+import { ref } from "vue";
+import { API } from "../config/config.js";
 
-  const newDocument = ref(
-    {
-      title: "",
-      content: ""
-    }
-  );
+const title = ref("");
+const content = ref("");
 
-  async function createDocument (e) {
-    console.log(e.target);
-    console.log(e.target.title.value);
-
-    newDocument.value.title = e.target.title.value;
-    newDocument.value.content = e.target.content.value;
-
-    console.log(newDocument.value);
-
-    const result = await fetch(`${API}/document`, {
-      body: JSON.stringify({
-        title: `${newDocument.value.title}`,
-        content: `${newDocument.value.content}`,
-      }),
-      headers: {
-        'content-type': 'application/json'
-        },
-      method: 'POST'
-    })
-    console.log(result);
-  }
+async function createDocument() {
+  await fetch(`${API}/document`, {
+    body: JSON.stringify({
+      title: `${title.value}`,
+      content: `${content.value}`,
+    }),
+    headers: {
+      "content-type": "application/json",
+    },
+    method: "POST",
+  });
+}
 </script>
 
 <template>
   <form @submit.prevent="createDocument" id="create">
-
     <label for="title">
-      <input type="text" id="title" name="title" required>
+      <input type="text" id="title" name="title" v-model="title" required />
     </label>
-
     <label for="content">
-      <textarea type="text" id="content" name="content" rows="5" cols="33" required></textarea>
+      <textarea
+        type="text"
+        id="content"
+        name="content"
+        rows="5"
+        cols="33"
+        v-model="content"
+        required
+      ></textarea>
     </label>
 
     <button form="create" value="submit">Submit</button>
   </form>
 </template>
-
 
 <style scoped>
 form {
