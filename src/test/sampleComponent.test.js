a1477f27243576dcf4d84d4778d8058326f0fb90
Kristoffer <kristoffer.ohlund@icloud.com>
2025-09-29 16:50:35 +0200

    Update sample tests

--- src/test/sampleComponent.test.js
+++ src/test/sampleComponent.test.js
@@ -2,16 +2,29 @@
  * E2E testing using vitest
  */
 
-import { render } from '@testing-library/vue'
+import { mount } from '@vue/test-utils'
 import sampleComponent from '../components/sampleComponent.vue'
+import DocItem from '../components/DocItem.vue'
+import { describe } from 'node:test'
 
-test('Hello world', () => {
-    const { getByText } = render(sampleComponent, {
-        props: {
-            msg: "string"
-        },
+
+
+describe("Testing display message"); {
+    test('displays message', () => {
+        const wrapper = mount(DocItem, {
+            props: {
+                doc: { _id: 55, title: "titel", content: "hejsan" }
+            }
+        }
+        )
+
+        // Assert the rendered text of the component
+        const title = wrapper.find("#title");
+        const content = wrapper.find("#content");
+        const id = wrapper.find("#id");
+
+        expect(title.element.textContent).toBe("titel")
+        expect(content.element.textContent).toBe("hejsan")
+        expect(id.element.textContent).toBe("55")
     })
-
-    // assert output
-    getByText("Hello")
-})
+}
