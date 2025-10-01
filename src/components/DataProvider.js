1ab2483cd187c17632acef1886b8500fa4acaa0a
Kristoffer <kristoffer.ohlund@icloud.com>
2025-10-01 14:46:36 +0200

    Add Dataprover composable

---
+++ src/components/DataProvider.js
@@ -0,0 +1,176 @@
+// <!--<template>
+//   <slot :data="data" :loading="loading"></slot>
+// </template>-->
+
+// <script setup>
+import { ref, reactive } from "vue";
+import { API } from "../config/config.js";
+
+const dataCalls = {
+  /**
+   * Get all Documents
+   * @returns Object, loading
+   */
+  getDocs: function getDocuments() {
+    const data = reactive({
+      doc: [
+        // {
+        //   _id: null,
+        //   title: null,
+        //   content: null,
+        // },
+      ],
+    });
+
+    const loading = ref(false);
+
+    const fetchDocuments = async () => {
+      loading.value = true;
+
+      const response = await fetch(API);
+      const responseData = await response.json();
+
+      data.doc = responseData;
+
+      loading.value = false;
+    };
+
+    fetchDocuments();
+
+    return {
+      data, loading
+    };
+  },
+
+  /**
+   * Get Single Document
+   * @param {*} docId 
+   * @returns 
+   */
+  getOneDoc: function getOneDoc(docId) {
+    const id = docId;
+    const data = reactive({
+      doc: [],
+    });
+
+    const loading = ref(false);
+
+    const fetchDocuments = async (id) => {
+      loading.value = true;
+
+      try {
+        const response = await fetch(`${API}/document/${id}`);
+        const responseData = await response.json();
+
+        loading.value = false;
+        if (responseData) {
+          data.doc = [responseData]
+        }
+        else data.doc = null;
+      }
+
+      catch (error) {
+        return { error, loading };
+      }
+    };
+
+    fetchDocuments(id);
+
+    return {
+      data, loading
+    };
+  },
+
+  /**
+   * Delete document
+   */
+  deleteOne: function deleteOne(docId) {
+    const id = docId;
+    // const data = reactive({
+    //   doc: [],
+    // });
+
+    const loading = ref(false);
+
+    const fetchDocuments = async (id) => {
+      loading.value = true;
+
+      try {
+        const response = await fetch(`${API}/document/`, {
+          body: JSON.stringify({ id: `${id}` }),
+          headers: {
+            "content-type": "application/json"
+          },
+          method: "DELETE"
+        });
+
+        const responseData = await response.json();
+
+        // loading.value = false;
+
+        if (responseData) {
+          data.doc = this.getDocs()
+        }
+        else data.doc = null;
+      }
+
+      catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchDocuments(id);
+
+    // return {
+    //   data, loading
+    // };
+  },
+
+  /**
+   * Alternative function
+   * @returns Get all documents
+   */
+  getAll: async function getAll() {
+
+    const loading = ref(false);
+
+    loading.value = true;
+
+    const response = await fetch(API);
+    const responseData = await response.json();
+
+    let data = this.getData(responseData);
+    // return (this.getData(responseData));
+
+    loading.value = false;
+
+    return { data, loading };
+  },
+
+  /**
+   * Reuse method to make code more dry
+   * @param {json} responseData parsed data from fetch
+   */
+  getData: function getData(responseData) {
+    // const loading = ref(false);
+
+    // loading.value = true;
+    const data = reactive({
+      doc: [
+        {
+          _id: null,
+          title: null,
+          content: null,
+        },
+      ],
+    });
+
+    // loading.value = false;
+    data.doc = responseData;
+
+    return data;
+  }
+}
+
+// </script >
+export default dataCalls;
--- src/views/DocumentsView.vue
+++ src/views/DocumentsView.vue
@@ -1,25 +1,44 @@
-<script setup lang="ts">
-import DocumentList from '../components/DocumentList.vue'
+<script setup>
+// Delete Document
+async function deleteDocument(id) {
+  console.log(id);
+  await dataCalls.deleteOne(id);
+  data.value = await dataCalls.getDocs();
+
+  await nextTick();
+
+  data.value = await dataCalls.getDocs();
+}
+
+// Redirect to update document
+async function updateDocument(id) {
+  router.push({
+    name: "update",
+    params: {
+      id: id,
+    },
+  });
+}
+
+import DocItem from "@/components/DocItem.vue";
+import dataCalls from "../components/DataProvider";
+import { ref, nextTick } from "vue";
+
+// const { data, loading } = dataCalls.deleteOne("68db823a95c2def26cc801d5");
+const dataObject = dataCalls.getDocs();
+
+const data = ref(dataObject.data);
+const loading = ref(dataObject.loading);
 </script>
 
 <template>
-  <div class="document-list">
-    <h1>All documents</h1>
-      <DocumentList />
+  <div class="joke-section">
+    <p v-if="loading">Documents is loading...</p>
+    <p v-if="data.doc === null">Something went wrong..</p>
+    <li v-else v-for="doc in data.doc" :key="doc._id">
+      <DocItem :doc="doc"> </DocItem>
+      <button :value="doc._id" @click="deleteDocument(doc._id)">Delete</button>
+      <button :value="doc._id" @click="updateDocument(doc._id)">Update</button>
+    </li>
   </div>
 </template>
-
-<style>
-.document-list {
-  margin: 0 auto;
-}
-@media (min-width: 1024px) {
-  .document-list {
-    min-height: 100vh;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    justify-content: center;
-  }
-}
-</style>
