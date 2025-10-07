5b28190ca56863ef3d87f5e55644b201d73cf7ec
SIMON-5000 <sidr24@student.bth.com>
2025-10-07 20:37:37 +0200

    make further socket tests

--- src/socket.js
+++ src/socket.js
@@ -9,10 +9,12 @@ export const state = reactive({
 
 // Porten måste vara densamma som socket i servern lyssnar på!
 // const URL = "http://localhost:3000";
-const URL = "https://jsramverk-sidr24.azurewebsites.net/";
+const URL = "https://jsramverk-sidr24.azurewebsites.net";
 
-export const socket = io(URL);
-console.log(socket);
+export const socket = io(URL, {
+  transports: ["websocket"],
+});
+console.log("socket: ", socket);
 // // The socket object can also be initialized without connecting right away with the autoConnect option:
 // // This can be useful for example when the user must provide some credentials before connecting.
 // export const socket = io(URL, {
@@ -35,4 +37,8 @@ socket.on("chat message", (...args) => {
 
 socket.on("bar", (...args) => {
   state.barEvents.push(args);
+});
+
+socket.on("test", (mess) => {
+  console.log(mess);
 });
