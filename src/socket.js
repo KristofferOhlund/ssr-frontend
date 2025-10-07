d26079ffa2cdfffcbe5f974a100274171bf7c862
SIMON-5000 <sidr24@student.bth.com>
2025-10-07 15:49:10 +0200

    change url to localhost

--- src/socket.js
+++ src/socket.js
@@ -8,8 +8,8 @@ export const state = reactive({
 });
 
 // Porten måste vara densamma som socket i servern lyssnar på!
-// const URL = "http://localhost:3000";
-const URL = "https://jsramverk-sidr24.azurewebsites.net/";
+const URL = "http://localhost:3000";
+// const URL = "https://jsramverk-sidr24.azurewebsites.net/";
 
 export const socket = io(URL);
 // // The socket object can also be initialized without connecting right away with the autoConnect option:
