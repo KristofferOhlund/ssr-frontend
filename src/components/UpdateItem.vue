b8526ec4e803bf85cc8a687d4f97a50efa4dbef3
Kristoffer <kristoffer.ohlund@icloud.com>
2025-10-07 14:49:50 +0200

    Added socket update emit and eventListener

--- src/components/UpdateItem.vue
+++ src/components/UpdateItem.vue
@@ -2,6 +2,7 @@
 import { onMounted, ref } from "vue";
 import { API } from "../config/config.js";
 import router from "../router/index.js";
+import { socket } from "@/socket";
 
 const props = defineProps({
   // data: Object,
@@ -10,12 +11,27 @@ const props = defineProps({
 
 const documentData = ref(null);
 
+// ------- SOCKET --------
+socket.on("update", (data) => {
+  console.log("Nu får client ett update event", data);
+  documentData.value.id = data.id;
+  documentData.value.title = data.title;
+  documentData.value.content = data.content;
+});
+
 // Call getDocument function
-onMounted(async () => await getDocument(props.id));
+onMounted(async () => {
+  await getDocument(props.id);
+  socket.emit("create", props.id);
+});
 
 async function getDocument(id) {
   const res = await fetch(`${API}/document/${id}`);
   documentData.value = await res.json();
+}
+
+function emit(value) {
+  socket.emit("update", documentData.value);
 }
 
 /**
@@ -45,19 +61,26 @@ async function updateDocument() {
 <template>
   <form v-if="documentData" @submit.prevent="updateDocument" id="update">
     <label for="title">Title</label>
-      <input type="text" id="title" name="title" v-model="documentData.title" required />
+    <input
+      type="text"
+      id="title"
+      name="title"
+      v-model="documentData.title"
+      @change="emit(documentData.title)"
+      required
+    />
 
     <label for="content">Content</label>
-      <textarea
-        type="text"
-        id="content"
-        name="content"
-        rows="20"
-        cols="50"
-        resize="none"
-        v-model="documentData.content"
-        required
-      ></textarea>
+    <textarea
+      type="text"
+      id="content"
+      name="content"
+      rows="20"
+      cols="50"
+      resize="none"
+      v-model="documentData.content"
+      required
+    ></textarea>
     <p>Make updates and press submit:</p>
     <button form="update" value="submit">Submit</button>
   </form>
