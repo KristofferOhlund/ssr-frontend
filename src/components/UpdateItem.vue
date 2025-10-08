f4adbd37e0c6bec89690995d37682799bc3cf42b
Kristoffer <kristoffer.ohlund@icloud.com>
2025-10-08 14:08:18 +0200

    Update emit handling

--- src/components/UpdateItem.vue
+++ src/components/UpdateItem.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import { onMounted, ref } from "vue";
+import { onMounted, ref, watch, watchEffect } from "vue";
 import { API } from "../config/config.js";
 import router from "../router/index.js";
 import { socket } from "@/socket";
 
+// Get's ID from parent (documentList)
 const props = defineProps({
   // data: Object,
   id: String,
@@ -13,30 +14,32 @@ const documentData = ref(null);
 
 // ------- SOCKET --------
 socket.on("update", (data) => {
-  console.log("Nu får client ett update event", data);
-  documentData.value.id = data.id;
-  documentData.value.title = data.title;
-  documentData.value.content = data.content;
+  documentData.value = data;
 });
 
+/**
+ * Emits the Update event
+ * @param value string Updated value
+ */
+function emit() {
+  socket.emit("update", documentData.value);
+}
+
 // Call getDocument function
+// emit the create event - backend creates room based on id.
 onMounted(async () => {
   await getDocument(props.id);
   socket.emit("create", props.id);
 });
 
+// Fetch all documents
 async function getDocument(id) {
   const res = await fetch(`${API}/document/${id}`);
   documentData.value = await res.json();
 }
 
-function emit(value) {
-  console.log("change, emit", value);
-  socket.emit("update", documentData.value);
-}
-
 /**
- * Createa new Document in database
+ * Update document in database
  */
 async function updateDocument() {
   await fetch(`${API}/document`, {
@@ -58,7 +61,7 @@ async function updateDocument() {
 }
 </script>
 
-<!-- TEMPLATE -->
+<!-- TEMPLATE @change="emit(documentData.title)"  @change="emit()"-->
 <template>
   <form v-if="documentData" @submit.prevent="updateDocument" id="update">
     <label for="title">Title</label>
@@ -67,7 +70,7 @@ async function updateDocument() {
       id="title"
       name="title"
       v-model="documentData.title"
-      @change="emit(documentData.title)"
+      @change="emit()"
       required
     />
 
@@ -80,6 +83,7 @@ async function updateDocument() {
       cols="50"
       resize="none"
       v-model="documentData.content"
+      @change="emit()"
       required
     ></textarea>
     <p>Make updates and press submit:</p>
