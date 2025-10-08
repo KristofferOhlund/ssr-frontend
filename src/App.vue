d0d3f7b6b12a70a7d35fdc17a2dd3a283d8f49fe
Kristoffer <kristoffer.ohlund@icloud.com>
2025-10-08 15:02:28 +0200

    Remove HelloWorld component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-import { state } from "./socket.js";
-console.log(state);
+import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
@@ -10,13 +7,14 @@ console.log(state);
     <img alt="Vue logo" class="logo" src="@/assets/logo.png" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="SSR - Frontend" />
-
+      <div class="greetings">
+        <h1 class="green">SSR - Frontend</h1>
+        <h3>Welcome to the SSR Editor</h3>
+      </div>
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/documents">Documents</RouterLink>
         <RouterLink to="/create">Create new doc</RouterLink>
-        <RouterLink to="/socket">socket</RouterLink>
         <RouterLink to="/connection-state">Connection</RouterLink>
       </nav>
     </div>
@@ -86,5 +84,26 @@ nav a:first-of-type {
     padding: 1rem 0;
     margin-top: 1rem;
   }
+
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
 }
 </style>
