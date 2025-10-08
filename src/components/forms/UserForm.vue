2f168c70bb32e8543a00c5f7c2bd875f1ffbf2fd
Kristoffer <kristoffer.ohlund@icloud.com>
2025-10-08 21:17:58 +0200

    dynamically handle /login and /register based on current path

--- src/components/forms/UserForm.vue
+++ src/components/forms/UserForm.vue
@@ -1,24 +1,62 @@
 <script setup>
-import { ref, onMounted, onBeforeUnmount } from 'vue'
+import { ref } from 'vue'
+import { API } from "../../config/config.js";
 import { useRoute } from 'vue-router'
-const route = useRoute()
+
+// Dynamic route handler
+// route = current route aka /login | /register
+let route = useRoute()
+
+const user = ref({
+    userName: "",
+    password: ""
+})
+
+// kalle
+// password123
 
 async function handleUser() {
-  if (route.path === "#/login") {
-    // Login user
-  }
-  // Register user
+    const userObject = {
+        ...user.value
+    };
+
+    try {
+        const path = route.path.replace("/", "");
+        const response = await fetch(`${API}${path}`, {
+            body: JSON.stringify(userObject),
+            headers: {
+                'content-type': 'application/json'
+            },
+            method: 'POST'
+        });
+
+        if (response.status === 200) {
+            const result = await response.json();
+            console.log(result);
+
+            if (result.data?.type === "success") {
+                auth.token = result.data.token;
+                loggedIn = true;
+                console.log("DU har loggat in")
+                return "ok";
+            }
+        }
+
+        return "not ok";
+    } catch (error) {
+        console.error(error);
+    }
 }
 </script>
 
 <template>
   <form @submit.prevent="handleUser" id="handleUser">
     <label for="title" class="green">Username</label>
-    <input type="text" id="title" name="title" required />
+    <input type="text" id="title" name="title" v-model="user.userName"required />
     <label for="content" class="green">Email</label>
-    <input type="email" id="email" name="email" required></input>
-    <label for="password" class="green">Password</label>
-    <input type="password" id="password" name="password" required></input>
+    <!--<input type="email" id="email" name="email" required></input>
+    <label for="password" class="green">Password</label>-->
+    <input type="password" id="password" name="password" v-model="user.password" required></input>
     <button class="formButton" value="submit"> {{ route.path.replace("/", "").toUpperCase() }}</button>
   </form>
 </template>
