ed2711dd6f274f7654535f11b765127df9f57c6d
Kristoffer <kristoffer.ohlund@icloud.com>
2025-10-09 16:15:18 +0200

    Add onMounted -> if user is not logged in, redirect to home.

--- src/components/forms/LogOut.vue
+++ src/components/forms/LogOut.vue
@@ -1,6 +1,7 @@
 <script setup>
 import { User } from "../composables/UserComposable.js";
 import router from "../../router/index.js";
+import { onMounted } from "vue";
 
 /**
  * Logout user
@@ -9,11 +10,33 @@ async function logOut() {
   User.isLoggedIn = false;
   router.push({ name: "home" });
 }
+
+/**
+ * Redirect user if not logged in
+ */
+onMounted(() => {
+  if (!User.isLoggedIn) {
+    router.push({ name: "home" });
+  }
+});
 </script>
 
 <template>
   <form action="" @submit.prevent="logOut">
-    <h3>Are you sure you want to log out?</h3>
-    <button type="submit">Logout</button>
+    <div class="logout-container">
+      <h3>Are you sure you want to log out?</h3>
+      <button type="submit" class="btn btn-delete">Logout</button>
+    </div>
   </form>
 </template>
+
+<style>
+.logout-container {
+  margin: 2rem;
+  display: flex;
+  align-items: center;
+  /* justify-content: center; */
+  flex-direction: column;
+  gap: 1rem;
+}
+</style>
