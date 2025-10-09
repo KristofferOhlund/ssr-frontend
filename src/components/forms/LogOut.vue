fb675aa9a552e9b5a16546e4846d63cb1b0b79ce
Kristoffer <kristoffer.ohlund@icloud.com>
2025-10-09 15:49:46 +0200

    Add new component

---
+++ src/components/forms/LogOut.vue
@@ -0,0 +1,10 @@
+<script setup>
+async function logOUt() {}
+</script>
+
+<template>
+  <form action="" @submit.prevent="logOut">
+    <h3>Are you sure you want to log out?</h3>
+    <button type="submit">Logout</button>
+  </form>
+</template>
