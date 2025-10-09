138ff8cd070c05a5805abab51bbd38c3b0fca75c
Kristoffer <kristoffer.ohlund@icloud.com>
2025-10-09 15:50:19 +0200

    Update user to reactive insted of ref

--- src/components/composables/UserComposable.js
+++ src/components/composables/UserComposable.js
@@ -1,77 +1,11 @@
 import { ref, reactive } from "vue";
 import { API } from "../../config/config.js";
 
-
-export const User = ref({
+// reactive updates all views using User object
+export const User = reactive({
     isLoggedIn: false,
     name: "kalle",
     email: "",
     token: "",
     docs: []
 });
-
-// export default const User = {
-//     login: async function login(userObject) {
-//         const user = {
-//             ...userObject
-//         };
-
-//         try {
-//             const response = await fetch(`${API}/login`, {
-//                 body: JSON.stringify(user),
-//                 headers: {
-//                     'content-type': 'application/json'
-//                 },
-//                 method: 'POST'
-//             });
-
-//             if (response.status === 200) {
-//                 const result = await response.json();
-//                 console.log(result);
-
-//                 if (result.data?.type === "success") {
-//                     auth.token = result.data.token;
-//                     loggedIn = true;
-//                     console.log("DU har loggat in")
-//                     return "ok";
-//                 }
-//             }
-
-//             return "not ok";
-//         } catch (error) {
-//             console.error(error);
-//         }
-//     },
-
-//     register: async function register(userObject) {
-//         const user = {
-//             ...userObject
-//         };
-
-//         try {
-//             const response = await fetch(`${API}/login`, {
-//                 body: JSON.stringify(user),
-//                 headers: {
-//                     'content-type': 'application/json'
-//                 },
-//                 method: 'POST'
-//             });
-
-//             if (response.status === 200) {
-//                 const result = await response.json();
-//                 console.log(result);
-
-//                 if (result.data?.type === "success") {
-//                     auth.token = result.data.token;
-//                     loggedIn = true;
-//                     console.log("DU har loggat in")
-//                     return "ok";
-//                 }
-//             }
-
-//             return "not ok";
-//         } catch (error) {
-//             console.error(error);
-//         }
-//     }
-// }
