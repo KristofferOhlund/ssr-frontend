53c13503fd92d78d9d9d604111e9f027c937667e
Kristoffer <kristoffer.ohlund@icloud.com>
2025-10-18 00:03:11 +0200

    Update Comment - test

--- src/components/CommentItem.vue
+++ src/components/CommentItem.vue
@@ -8,82 +8,116 @@ import { ref, onMounted } from "vue";
 // commentRef.value = docComments
 // https://stackoverflow.com/questions/6328718/how-to-wrap-surround-highlighted-text-with-an-element
 
+// Ref för att auto incrementera ID
+const refId = ref(0);
 
+function makeComment() {
+  const selection = window.getSelection();
+  if (!selection) {
+    return;
+  }
 
-  function makeComment(){
-    const selection = window.getSelection();
-    if(!selection) {
-      return;
-    }
+  const range = selection.getRangeAt(0).cloneRange();
 
-    const range = selection.getRangeAt(0).cloneRange();
-
-    const commentEl = document.createElement("button");
-    commentEl.classList.add('yellow', "btn-comment");
-    commentEl.setAttribute("popovertarget", "testPop");
-  // ------------------------------------------------------------------------------------
-    // We send to DB and it returns the commentId
-    // Separate collection: _id, docId, comment, deleted ?
-
-    // HÄR SÄTTS ID:
-    // commentId = await addCommentToDatabase(documentID)
-    // span.id = commentId;
-    // popovertarget="`${commentId}`";
-
-
-    // For all comments with documentID -> <div id="`${commentId}`" popover>`${comment}`</div>
-    // update comments-ref
-    // socket emit
+  const commentEl = document.createElement("button");
+  commentEl.classList.add("yellow", "btn-comment");
+  commentEl.setAttribute("popovertarget", "testPop");
 
   // ------------------------------------------------------------------------------------
+  // We send to DB and it returns the commentId
+  // Separate collection: _id, docId, comment, deleted ?
 
-    range.surroundContents(commentEl);
-    selection.removeAllRanges();
-    selection.addRange(range)
-  };
+  // HÄR SÄTTS ID:
+  // commentId = await addCommentToDatabase(documentID)
+  // span.id = commentId;
+  // popovertarget="`${commentId}`";
 
+  // For all comments with documentID -> <div id="`${commentId}`" popover>`${comment}`</div>
+  // update comments-ref
+  // socket emit
 
+  // ------------------------------------------------------------------------------------
+
+  range.surroundContents(commentEl);
+  // selection.removeAllRanges();
+  // selection.addRange(range);
   // Testing popover concept:
   const testPopover = document.createElement("div");
-  testPopover.setAttribute('popover', "auto");
-  testPopover.classList.add("comment", "popover")
-  testPopover.id = "testPop";
-  testPopover.innerHTML = `
-    <h1>Hello!</h1>
-    <p>Previous comments:</p>
-    <div id="comment-list">jop</div>
-    <textarea id="comment-text"></textarea>
-    <button id="popSubmit">Skicka till DB</button>
-    `;
-
+  testPopover.setAttribute("popover", "auto");
+  testPopover.classList.add("popover");
+  testPopover.id = "popover";
   document.body.appendChild(testPopover);
 
-  // Add delete-comment-button (Soft delete)
-  // @click="deleteComment"
+  addComment();
+}
 
+// Adds a Comment div
+// Contains buttons and eventListeners
+function addComment() {
+  const testpop = document.querySelector("#popover");
 
-  onMounted(() => {
-    const popSubmit = document.querySelector("#popSubmit");
-    const commentList = document.querySelector("#comment-list")
-    const commentText = document.querySelector("#comment-text")
-    popSubmit.addEventListener('click', (event) => {
-      console.log(event);
-      console.log(commentText.value)
-      const newComment = document.createElement("p")
-      newComment.textContent = commentText.value;
+  const comment = document.createElement("div");
+  comment.setAttribute("id", refId.value);
+  comment.classList.add("comment");
+  const label = document.createElement("p");
+  label.innerText = "Comment created by: User";
+  const created = document.createElement("p");
+  created.innerText = "Comment created: date";
+  const textarea = document.createElement("textarea");
+  textarea.setAttribute("required", "true");
+  const addComBtn = document.createElement("button");
+  addComBtn.innerText = "Add Comment";
+  addComBtn.addEventListener("click", () => {
+    // disable comment
+    textarea.setAttribute("readonly", "true");
+    saveCommentInDB();
+    addComment();
+  });
 
-      commentList.appendChild(newComment);
+  const delComBtn = document.createElement("button");
+  delComBtn.innerText = "Resolve Comment";
+  delComBtn.addEventListener("click", () => {
+    resolveComment(comment);
+    delComBtn.removeEventListener("click", resolveComment);
+  });
 
-      console.log(commentList.innerHtml);
-    })
-  })
+  // add to comment div
+  comment.appendChild(label);
+  comment.appendChild(created);
+  comment.appendChild(textarea);
+  comment.appendChild(addComBtn);
+  comment.appendChild(delComBtn);
 
+  // add to testpop
+  testpop.appendChild(comment);
+
+  // increment ID
+  refId.value++;
+}
+
+// Save Comment in dB
+function saveCommentInDB() {
+  console.log("doc saved in DB");
+}
+
+// Resolve comment
+function resolveComment(comment) {
+  const p = document.createElement("p");
+  p.innerText = "Resolved at: <date>";
+
+  comment.insertBefore(p, comment.childNodes[2]);
+  console.log("doc saved in DB");
+  comment.setAttribute("disabled", "true");
+}
+
+// Add delete-comment-button (Soft delete)
+// @click="deleteComment"
 </script>
 
 <template>
   <div class="comments">
     <div class="textarea" id="comment" contenteditable="true"></div>
-    <button type="submit" @click="makeComment" >Add comment</button>
+    <button type="submit" @click="makeComment">Add comment</button>
   </div>
 </template>
 
@@ -97,14 +131,30 @@ import { ref, onMounted } from "vue";
   cursor: help;
 }
 
+.disabled {
+  background-color: grey;
+}
+
+.comment {
+  background-color: green;
+  padding: 1rem;
+  border-radius: 5px;
+}
+
 .popover {
   /* https://developer.mozilla.org/en-US/docs/Web/CSS/CSS_anchor_positioning ? */
   position: absolute;
   z-index: 999;
   margin: auto auto;
-  background-color: #ffffffee;
+  background-color: red;
   border: 2px solid var(--color-border);
   border-radius: 8px;
+  display: flex;
+  flex-direction: column;
+  gap: 1rem;
+  border-radius: 2px;
+  overflow-y: scroll;
+  height: 400px;
 }
 
 .textarea {
@@ -115,8 +165,8 @@ import { ref, onMounted } from "vue";
 }
 </style>
 
-
-// <!-- <script setup lang="ts">
+//
+<!-- <script setup lang="ts">
 //   import { ref } from 'vue'
 
 //   const title = ref('My content is pure text')
@@ -159,8 +209,8 @@ import { ref, onMounted } from "vue";
 // </template>
 //  -->
 
-
-// <!-- <script setup>
+//
+<!-- <script setup>
 // import { ref, onMounted } from "vue";
 
 // onMounted(() => {
@@ -192,4 +242,3 @@ import { ref, onMounted } from "vue";
 //   color: black;
 // }
 // </style> -->
-
