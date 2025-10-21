355c13eea4a8c9fa1e704c0c057a7ef8e357ab8d
Kristoffer <kristoffer.ohlund@icloud.com>
2025-10-21 13:34:09 +0200

    Remove console.log

--- src/components/composables/DocumentActions.js
+++ src/components/composables/DocumentActions.js
@@ -26,7 +26,6 @@ const DocActions = {
       }
 
       const responseData = await response.json();
-      console.log(responseData);
       return responseData;
     } catch (error) {
       console.error("fetchDocuments error:", error);
